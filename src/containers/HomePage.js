7b4ac95c93d9e594b619f1bf78e49c6eb7b66c05
Tanya Dubinchyk <tat.dbk@gmail.com>
2019-10-03 23:32:31 -0700

    Update HomePage.js

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import HomeIcon from '@material-ui/icons/Home';
 
 class HomePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            user: null
+        };
+    }
+
     render() {
         return (
             <div>
@@ -13,3 +20,4 @@ class HomePage extends Component {
 }
 
 export default HomePage;
+
