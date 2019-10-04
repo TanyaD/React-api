29b224fda9a7be8177b174be4cb6adfb918d5779
Tanya Dubinchyk <tat.dbk@gmail.com>
2019-10-03 23:33:17 -0700

    Update MoviePage.js

--- src/containers/MoviePage.js
+++ src/containers/MoviePage.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import MovieIcon from '@material-ui/icons/Movie';
 
 class MoviePage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: [],
+            searchQuery: ''
+        };
+    }
+
     render() {
         return (
             <div>
