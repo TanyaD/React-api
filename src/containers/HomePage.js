79d0a7f31bbbffff2bcc74c6b57bf9f2a76e5bfd
Tanya Dubinchyk <tat.dbk@gmail.com>
2019-10-15 13:59:01 -0700

    Update HomePage.js

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { Link } from 'react-router-dom';
 
 import SortBox from '../SortBox/SortBox';
-import styles from '../HomePage/HomePage.module.css';
 import Services from '../../services.js';
+import MovieList from '../MovieList/MovieList';
 
 class HomePage extends Component {
   constructor(props) {
@@ -32,43 +25,10 @@ class HomePage extends Component {
   }
 
   render() {
-    const listMovies = this.state.movies;
-    const myMovies = listMovies.map(item => (
-      <Grid
-        key={item.id}
-        item
-        component={Link}
-        to='/moviepage'
-        className={styles.grid}
-      >
-        <Card className={styles.card}>
-          <CardHeader
-            className={styles.cardhead}
-            title={item.title}
-            subheader={item.release_date}
-          />
-          <CardMedia
-            className={styles.media}
-            image={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${item.poster_path}`}
-            title='Movie'
-          />
-          <CardContent>
-            <Typography variant='body2' color='textSecondary' component='p'>
-              {item.overview}
-            </Typography>
-          </CardContent>
-        </Card>
-      </Grid>
-    ));
-
     return (
       <div>
         <SortBox count={this.state.count} />
-        <div className={styles.root}>
-          <Grid container className={styles.root} spacing={1}>
-            {myMovies}
-          </Grid>
-        </div>
+        <MovieList />
       </div>
     );
   }
