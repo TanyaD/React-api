d3006851eec6e9b511b970510b9c5b0fb458b25a
Tanya Dubinchyk <tat.dbk@gmail.com>
2019-10-18 22:43:23 -0700

    Update NotFound.js

--- src/containers/NotFound.js
+++ src/containers/NotFound.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
-/*import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-*/
-
 
 class NotFound extends Component {
     render() {
-        return (<div>404</div>)
+        return (<div>The page you are looking for doesn't exist</div>)
     }
 }
 
