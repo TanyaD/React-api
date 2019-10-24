b8e8a6d3a2b17f3d912370ac1b5a263787684334
Tanya Dubinchyk <tat.dbk@gmail.com>
2019-10-23 23:22:56 -0700

    Add Redux support with thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './containers/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./containers/App/App";
+import "./index.css";
+import reducer from "./reducers/reducer";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(<App store={store} />, document.getElementById("root"));
+
 serviceWorker.unregister();
