043e7c3679ecb811fbd432e97d27f6f81493108b
Tanya Dubinchyk <tat.dbk@gmail.com>
2019-10-27 23:38:31 -0700

    Update action.js

--- src/action.js
+++ src/action.js
@@ -1,27 +1,42 @@
-export const FETCH_MV_BEGIN = "FETCH_MV_BEGIN";
-export const FETCH_MV_SUCCESS = "FETCH_MV_SUCCESS";
-export const FETCH_MV_FAILURE = "FETCH_MV_FAILURE";
+export const SEARCH_MOVIES = "INCREMENT";
+export const search_movies = () => ({ type: SEARCH_MOVIES});
+export const FETCH_MOVIES_BEGIN = "FETCH_MOVIES_BEGIN";
+export const FETCH_MOVIES_SUCCESS = "FETCH_MOVIES_SUCCESS";
+export const FETCH_MOVIES_FAILURE = "FETCH_MOVIES_FAILURE";
 
+export const SEARCH_QUERY = "SEARCH_QUERY";
+export const setSearchQuery = (value) => ({ type: SEARCH_QUERY, query:value });
+
+export const OPEN_MOVIE_ID = "OPEN_MOVIE_ID";
+export const openMovieId = (movie_id) => ({ type: OPEN_MOVIE_ID, movie_id:movie_id });
+
+export const CLOSE_MODAL = "CLOSE_MODAL";
+export const closeModal = () => ({ type: CLOSE_MODAL });
+
+const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
+const HOST = 'https://api.themoviedb.org/3';
+const GET_MOVIES_URL = '/discover/movie';
+const SEARCH_MOVIES_URL = '/search/movie';
 export const fetchMVBegin = () => ({
-  type: FETCH_MV_BEGIN
+  type: FETCH_MOVIES_BEGIN
 });
 
 export const fetchMVSuccess = movies => ({
-  type: FETCH_MV_SUCCESS,
+  type: FETCH_MOVIES_SUCCESS,
   payload: movies
 });
 
 export const fetchMVFailure = error => ({
-  type: FETCH_MV_FAILURE,
+  type: FETCH_MOVIES_FAILURE,
   payload: { error }
 });
 
 export function fetchMV() {
-    const API_KEY=process.env.REACT_APP_MOVIE_API_KEY;
+  const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
   return async dispatch => {
     dispatch(fetchMVBegin());
     return fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2`
+      `${HOST}${GET_MOVIES_URL}?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2`
     )
       .then(res => res.json())
       .then(json => {
