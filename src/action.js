2fa3f0ec6217629975af6eca23f0f66b3e162aae
Tanya Dubinchyk <tat.dbk@gmail.com>
2019-11-06 16:53:21 -0800

    Update action.js

--- src/action.js
+++ src/action.js
@@ -5,19 +5,32 @@ export const FETCH_MOVIES_FAILURE = "FETCH_MOVIES_FAILURE";
 export const SEARCH_QUERY = "SEARCH_QUERY";
 export const OPEN_MOVIE_ID = "OPEN_MOVIE_ID";
 export const CLOSE_MODAL = "CLOSE_MODAL";
-
-export const setSearchQuery = value => ({ type: SEARCH_QUERY, query: value });
-export const search_movies = () => ({ type: SEARCH_MOVIES });
-export const closeModal = () => ({ type: CLOSE_MODAL });
-export const openMovieId = movie_id => ({
-    type: OPEN_MOVIE_ID,
-    movie_id: movie_id
-  });
+export const SET_MOVIES = "SET_MOVIES";
+export const UPDATE_TOTAL_RESULTS = "UPDATE_TOTAL_RESULTS";
+export const SET_MOVIE_INFO = "SET_MOVIE_INFO";
 
 const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
 const HOST = "https://api.themoviedb.org/3";
 const GET_MOVIES_URL = "/discover/movie";
 const SEARCH_MOVIES_URL = "/search/movie";
+
+export const setSearchQuery = value => ({ type: SEARCH_QUERY, query: value });
+export const setMovielist = newMovies => ({
+  type: SET_MOVIES,
+  movies: newMovies
+});
+export const viewMovieInfo = id => ({ type: SET_MOVIE_INFO, id: id });
+export const search_movies = () => ({ type: SEARCH_MOVIES });
+export const closeModal = () => ({ type: CLOSE_MODAL });
+
+export const openMovieId = filteredMovie => ({
+  type: OPEN_MOVIE_ID,
+  filteredMovie: filteredMovie,
+  title: filteredMovie[0].title,
+  overview: filteredMovie[0].overview,
+  release_date: filteredMovie[0].release_date,
+  poster_path: filteredMovie[0].poster_path
+});
 
 export const fetchMovieBegin = () => ({
   type: FETCH_MOVIES_BEGIN
@@ -33,6 +46,11 @@ export const fetchMovieFailure = error => ({
   payload: { error }
 });
 
+export const updateMovieResults = totalResults => ({
+  type: UPDATE_TOTAL_RESULTS,
+  payload: totalResults
+});
+
 export function fetchMovie() {
   const API_KEY = process.env.REACT_APP_MOVIE_API_KEY;
   const url = new URL(`${HOST}${GET_MOVIES_URL}?api_key=${API_KEY}`),
@@ -44,7 +62,9 @@ export function fetchMovie() {
       .then(res => res.json())
       .then(json => {
         const movies = json.results;
+        const totalResults = json.total_results;
         dispatch(fetchMovieSuccess(movies));
+        dispatch(updateMovieResults(totalResults));
         Object.keys(params).forEach(key =>
           url.searchParams.append(key, params[key])
         );
@@ -59,6 +79,56 @@ export const searchMovies = (query, language, page) => {
     dispatch(fetchMovieBegin());
     return fetch(
       `${HOST}${SEARCH_MOVIES_URL}?api_key=${API_KEY}&language=${language}&page=${page}&query=${query}`
+    )
+      .then(res => res.json())
+      .then(json => {
+        const movies = json.results;
+        dispatch(fetchMovieSuccess(movies));
+        return json;
+      })
+      .catch(error => dispatch(fetchMovieFailure(error)));
+  };
+};
+
+export const searchByDirector = query => {
+  return async dispatch => {
+    dispatch(fetchMovieBegin());
+    return fetch(
+      `${HOST}/search/person?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=${query}`
+    )
+      .then(res => res.json())
+      .then(json => {
+        const movies = json.results[0].known_for;
+        dispatch(fetchMovieSuccess(movies));
+        return json;
+      })
+      .catch(error => dispatch(fetchMovieFailure(error)));
+  };
+};
+
+export function sortMovie(sort_by) {
+  return async dispatch => {
+    dispatch(fetchMovieBegin());
+    return fetch(
+      `${HOST}${GET_MOVIES_URL}?api_key=${API_KEY}&language=en-US&sort_by=${sort_by}`
+    )
+      .then(res => res.json())
+      .then(json => {
+        const movies = json.results;
+        const totalResults = json.total_results;
+        dispatch(fetchMovieSuccess(movies));
+        dispatch(updateMovieResults(totalResults));
+        return json;
+      })
+      .catch(error => dispatch(fetchMovieFailure(error)));
+  };
+}
+
+export const nextPage = pageNumber => {
+  return async dispatch => {
+    dispatch(fetchMovieBegin());
+    return fetch(
+      `${HOST}${GET_MOVIES_URL}?api_key=${API_KEY}&page=${pageNumber}`
     )
       .then(res => res.json())
       .then(json => {
