f1063c1ebf0d96c7e990c4790f1a09e4e6e58774
Tanya Dubinchyk <tat.dbk@gmail.com>
2019-11-06 16:54:02 -0800

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -2,10 +2,20 @@ const initialState = {
   loading: false,
   count: 0,
   movies: [],
-  searchQuery:'',
-  movie_id:0,
-  open: false
+  searchQuery: "",
+  movie_id: 0,
+  open: false,
+  language: "en-US",
+  sort_by: "primary_release_date.asc",
+  include_adult: false,
+  page: 1,
+  include_video: false,
+  isOldestFirst: false,
+  totalResults: 0,
+  currentPage: 1,
+  currentMovie: null
 };
+
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case "FETCH_MOVIES_BEGIN":
@@ -14,12 +24,17 @@ export default function reducer(state = initialState, action) {
         loading: true,
         error: null
       };
-
     case "FETCH_MOVIES_SUCCESS":
       return {
         ...state,
         loading: false,
-        movies: action.payload
+        movies: action.payload,
+        count: action.payload.length
+      };
+    case "UPDATE_TOTAL_RESULTS":
+      return {
+        ...state,
+        totalResults: action.payload
       };
 
     case "FETCH_MOVIES_FAILURE":
@@ -29,23 +44,34 @@ export default function reducer(state = initialState, action) {
         error: action.payload.error,
         movies: []
       };
-
-      case "SEARCH_QUERY":
-          return {
-            ...state,
-            searchQuery: action.query,
-          };
-      case "OPEN_MOVIE_ID":
-          return {
-            ...state,
-            movie_id: action.movie_id,
-            open: true
-          };
-          case "CLOSE_MODAL":
-            return{
-              ...state,
-              open: false
-            };
+    case "SEARCH_QUERY":
+      return {
+        ...state,
+        searchQuery: action.query
+      };
+    case "OPEN_MOVIE_ID":
+      return {
+        ...state,
+        currentMovie: action.filteredMovie,
+        title: action.title,
+        overview: action.overview,
+        release_date: action.release_date,
+        poster_path: action.poster_path,
+        open: true
+      };
+    case "CLOSE_MODAL":
+      return {
+        ...state,
+        open: false
+      };
+    case "SET_MOVIES":
+      return {
+        movies: action.movies
+      };
+    case "SET_MOVIE_INFO":
+      return {
+        ...state
+      };
     default:
       return state;
   }
